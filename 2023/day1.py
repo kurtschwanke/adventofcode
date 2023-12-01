cf2aa222de76acde75330fa4e9a9a07fe03833a9
Kurt Schwanke <kurt.schwanke@gmail.com>
2023-12-01 11:01:04 -0600

    compact

--- 2023/day1.py
+++ 2023/day1.py
@@ -7,17 +7,7 @@ lines = file.readlines()
 
 reg = re.compile("(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))", re.IGNORECASE)
 
-number_dict = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9'
-}
+number_dict = {'one': '1','two': '2','three': '3','four': '4','five': '5','six': '6','seven': '7','eight': '8','nine': '9'}
 
 total = 0
 for line in lines:
@@ -33,4 +23,3 @@ for line in lines:
     # print("Line: ", line.rstrip(), "\nNums:", nums, "\nNumber: ",number, "\n")
     total += int(number)
 print("Total: ", total)
-
