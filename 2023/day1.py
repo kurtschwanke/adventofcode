c8f35e11e353a8336353b706fb9b376691fd581f
Kurt Schwanke <kurt.schwanke@gmail.com>
2023-12-01 10:49:41 -0600

    fixed regex

--- 2023/day1.py
+++ 2023/day1.py
@@ -27,10 +27,7 @@ number_dict = {
 
 total = 0
 for line in lines:
-    print(line)
     nums = re.findall(reg, line.rstrip())
-    print(re.findall(reg, line))
-    # break
     index = [0, len(nums)-1]
     firstNum = nums[index[0]]
     secondNum = nums[index[1]]
@@ -39,7 +36,7 @@ for line in lines:
     if not re.match(regexDigit, secondNum):
         secondNum = ''.join(number_dict[ele] for ele in secondNum.split())
     number = firstNum + secondNum
-    print("Line: ", line.rstrip(), "\nNums:", nums, "\nNumber: ",number, "\n")
+    # print("Line: ", line.rstrip(), "\nNums:", nums, "\nNumber: ",number, "\n")
     total += int(number)
 print("Total: ", total)
 
